cfedf48268a2388d3a905fd3cc7e351e3a17c818
cj <shahid.alam57@gmail.com>
2022-03-08 10:19:42 +0530

    second commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,14 @@ import {AppBar,ToolBar,Container,Typography,createTheme,MenuItem,ThemeProvider,S
 export default function Header() {
 const theme = createTheme({
   typography: {
+
     subtitle1: {
     fontFamily:"'Poppins', sans-serif",
       fontSize: 20,
+      fontWeight:'bold',
+      cursor: 'pointer',
+      flex:1,
+      color:'gold'
     }
   },
 });
@@ -13,16 +18,16 @@ const theme = createTheme({
 		<ThemeProvider theme={theme}>
 		<AppBar color='transparent' position='static'>
 	{/*container makes the navbar responsive*/}
-		<Container>
+		<Container sx={{display : 'flex', justifyContent :'space-between'}}>
 		<Typography sx={{ m: 1 }} variant="subtitle1">
 		CryptoWeb
 		</Typography>
 	{/*this is the select menu option*/}
 		<Select variant='outlined'>
-			<MenuItem>
+			<MenuItem value={'INR'}>
 				INR
 			</MenuItem>
-			<MenuItem>
+			<MenuItem value={'USD'}>
 				USD
 			</MenuItem>
 		</Select>
