e139a186cec683bb87f81697756f3fc8cd8991b0
cj <shahid.alam57@gmail.com>
2022-03-12 10:33:16 +0530

    added theme properties with sx

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,19 +1,31 @@
 import React from 'react'
 import {makeStyles} from '@mui/styles'
-
+import { createTheme, ThemeProvider, Box} from '@mui/material';
 //this is used fo making custom CSS
-const useStyles=makeStyles(()=>({
-	banner:{
-		backgroundImage :"url(../images/bg.jpg)"
-	}	
-}))
+/*
+put a picture in the component
+
+*/
+
+const theme = createTheme()
+
+
 
 function Banner() {
-	const classes = useStyles()
+	const styles = theme =>({
+
+	})
 	return (
-		<div className={classes.banner}>  
-			
-		</div>
+		<ThemeProvider theme={theme}>
+		<Box sx={{
+			display:'flex',
+			height :'10vh',
+			width:'100%',
+			backgroundImage: 'url(https://images.unsplash.com/photo-1639322537504-6427a16b0a28?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80)',
+
+		}}>  
+		</Box>
+		</ThemeProvider>
 	)
 }
 
