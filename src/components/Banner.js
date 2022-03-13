3b0a603a2d1691be9f9a80a95afd30a78f556a31
cj <shahid.alam57@gmail.com>
2022-03-13 20:26:15 +0530

    banner commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import {makeStyles} from '@mui/styles'
-import { createTheme, ThemeProvider, Box} from '@mui/material';
+import { createTheme, ThemeProvider, Box, Typography} from '@mui/material';
 //this is used fo making custom CSS
 /*
 put a picture in the component
@@ -12,18 +11,36 @@ const theme = createTheme()
 
 
 function Banner() {
-	const styles = theme =>({
-
-	})
 	return (
 		<ThemeProvider theme={theme}>
 		<Box sx={{
 			display:'flex',
-			height :'10vh',
+			flexDirection: 'column',
+			height :'20vh',
+			paddingBottom : '1rem',
 			width:'100%',
 			backgroundImage: 'url(https://images.unsplash.com/photo-1639322537504-6427a16b0a28?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80)',
-
-		}}>  
+			backgroundPosition : 'center center',
+			justifyContent: 'center',
+			alignItems : 'center',
+			backdropFilter: 'blur',
+			[theme.breakpoints.down('md')]: {
+      		height: '15vh'
+    },
+		}}> 
+		<Typography sx={{
+			height: '20vh',
+			display:'flex',
+			alignItems: 'center',
+			fontSize : '3rem',
+			fontWeight: '800',
+		}}>CryptoWeb
+		</Typography> 
+		<Typography sx={{
+			color: '#d6c8c7',
+		}}>
+			Track your Crypto Holdings for FREE
+		</Typography>
 		</Box>
 		</ThemeProvider>
 	)
